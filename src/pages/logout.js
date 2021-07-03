739c9b61c526c30faf099bb5fa60238b71e31669
Onur <onurr.yazici@outlook.com>
2021-07-03 09:55:53 +0300

    .

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -6,7 +6,7 @@ export default function Logout() {
     const token = localStorage.getItem("user-token")
     const history = useHistory();
     useEffect(() => {
-        axios.get(`http://192.168.91.130:3030/api/secured/logout?token=${token}`)
+        axios.get(`http://192.168.91.130:3030/api/open-service/logout?token=${token}`)
         .then((response)=>{
             history.push("/login")
         })
